35a4aed159881a44dff134836fa4d6900cf3a6c5
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-02-25 17:19:16 +0100

    add notify toastr default scripts and styles

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,9 +18,17 @@ class Configuration implements ConfigurationInterface
             ->children()
                 ->arrayNode('scripts')
                     ->scalarPrototype()->end()
+                    ->defaultValue([
+                        'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+                        'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js',
+                    ])
                 ->end()
                 ->arrayNode('styles')
                     ->scalarPrototype()->end()
+                    ->defaultValue([
+                        'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css',
+                        'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css'
+                    ])
                 ->end()
                 ->arrayNode('options')
                     ->ignoreExtraKeys(false)
